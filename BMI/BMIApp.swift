624407e3a8455d7e66278a4d7f270c936fcdd303
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-09 18:19:27 +0900

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by 佐藤幸久 on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
