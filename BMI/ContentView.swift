228249f1976e0a499a4017ec5a3bccf64aee6443
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-09 21:47:26 +0900

    入力エリア作成

--- BMI/ContentView.swift
+++ BMI/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var height: String = ""
+    @State private var heightUnit: String = "meter"
+    @State private var weight: String = ""
+    let heightUnits = ["meter", "centimeter"]
+
     var body: some View {
         Form {
-            
+            Section("入力") {
+                HStack {
+                    TextField("身長", text: $height)
+                        .keyboardType(.decimalPad)
+                    Picker("", selection: $heightUnit) {
+                        ForEach(heightUnits, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                }
+                HStack {
+                    TextField("体重", text: $weight)
+                        .keyboardType(.decimalPad)
+                    Text("kg")
+                }
+            }
         }
     }
 }
